adacc2b9e3ec3cf720b74b7f090bb76bfa5691c5
coderRaj07 <12131501.edu@gmail.com>
2026-01-21 19:48:13 +0530

    FEAT: added joins for faster queries

--- app/models/cart.py
+++ app/models/cart.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, ForeignKey, CheckConstraint
 from app.core.database import Base
+from sqlalchemy.orm import relationship
 
 class CartItem(Base):
     __tablename__ = "cart_items"
@@ -7,6 +8,8 @@ class CartItem(Base):
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     product_id = Column(Integer, ForeignKey("products.id"), nullable=False)
     quantity = Column(Integer, nullable=False)
+
+    product = relationship("Product")
 
     __table_args__ = (
         CheckConstraint("quantity > 0", name="quantity_positive"),
